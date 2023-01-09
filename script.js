3100544676f00214aa3fe3eaa704d29a70c4e098
Ag <theankushgautam@gmail.com>
2023-01-09 21:47:25 +0545

    added projects slider using swiper js

--- script.js
+++ script.js
@@ -49,3 +49,15 @@ function progressBar() {
 }
 
 progressBar();
+
+//Initialize Swiper--
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
